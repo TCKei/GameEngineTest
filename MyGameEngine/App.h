fd4402d6dd781d741386ef7ccd7fa2779cc6c719
TCKei <keisei19990924@gmail.com>
2022-03-31 00:00:52 +0900

    Create

---
+++ MyGameEngine/App.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <Windows.h>
+#include<tchar.h>
+#include<memory>
+#include"Engine.h"
+
+
+const UINT WINDOW_WIDTH = 1920;
+const UINT WINDOW_HEIGHT = 1080;
+void StartApp(const TCHAR* appName); // ‚±‚ê‚ğŒÄ‚ñ‚¾‚çƒAƒvƒŠ‚ªÀs‚·‚é‚æ‚¤‚É‚·‚é
+
